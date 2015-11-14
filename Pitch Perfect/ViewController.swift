62a7f74b09437558531dffbd5b142c25cb971c35
tkolleh <tkolleh@me.com>
2015-11-13 23:52:27 -0500

    add navigation controller for voice augment screen

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -12,10 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopRecordingBtn: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingBtn.hidden = true;
+        recordButton.enabled = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,19 +31,17 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record the user's voice
-        if recordingLabel.hidden {
-            recordingLabel.hidden = false
-            stopRecordingBtn.hidden = false
-            print("begin recording")
-        }
+        recordButton.enabled = false
+        recordingLabel.hidden = false
+        stopRecordingBtn.hidden = false
+        print("begin recording")
     }
 
     @IBAction func stopRecording(sender: UIButton) {
-        if !recordingLabel.hidden {
-            recordingLabel.hidden = true
-            stopRecordingBtn.hidden = true
-            print("end recording")
-        }
+        recordingLabel.hidden = true
+        stopRecordingBtn.hidden = true
+        recordButton.enabled = true
+        print("end recording")
     }
 }
 
